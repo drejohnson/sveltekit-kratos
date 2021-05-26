168457f1a0a94cffa2ad4888607e80eba88cc0df
DeAndre Johnson <dre@phreshr.com>
2021-05-26 08:59:43 +0800

    clean up; added logout button

--- src/routes/dashboard.svelte
+++ src/routes/dashboard.svelte
@@ -1,7 +1,5 @@
 <script lang="ts" context="module">
 	import type { Load } from '@sveltejs/kit';
-	import { Configuration, PublicApi } from '@ory/kratos-client';
-	import config from '$lib/config';
 
 	export const load: Load = async ({ session }) => {
 		// console.log(session);
@@ -16,8 +14,10 @@
 </script>
 
 <script>
+	import { logoutUrl } from '$lib/helpers';
+
 	export let session;
-	console.log(session);
+	// console.log(session);
 </script>
 
 <h2>Dashboard</h2>
@@ -29,9 +29,12 @@
 	<h3>This example app is secure by default!</h3>
 	<p>Hello, nice to have you! You signed up with this data:</p>
 	<pre><code>{JSON.stringify(session.traits, null, 2)}</code></pre>
-	<p>Requests to your application contain a ORY Kratos Session Cookie:</p>
-	<pre><code>{session.headers}</code></pre>
 
 	<p>The decoded ORY Kratos Session payload is as follows:</p>
 	<pre><code>{JSON.stringify(session, null, 2)}</code></pre>
 </div>
+
+<a
+	class="flex items-center justify-center h-12 px-6 w-64 bg-blue-600 mt-8 rounded font-semibold text-sm text-blue-100 hover:bg-blue-700"
+	href={logoutUrl}>Logout</a
+>
