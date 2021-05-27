e3734ee18ec1ab45ffbf9a7a682dd21fdde0cde2
Dre Johnson <dre@phreshr.com>
2021-05-27 23:27:26 +0800

    refactored to input components

--- src/routes/auth/verify.svelte
+++ src/routes/auth/verify.svelte
@@ -35,16 +35,23 @@
 			return redirectOnError(error, '/self-service/verification/browser');
 		}
 	}
+
 </script>
 
 <script lang="ts">
 	import type { UiContainer } from '@ory/kratos-client';
 	import AuthForm from '$lib/Kratos/AuthForm.svelte';
-	import Input from '$lib/Kratos/Input.svelte';
 	import Message from '$lib/Kratos/Message.svelte';
 	import AuthContainer from '$lib/Kratos/AuthContainer.svelte';
+	import { getUiNodes } from '$lib/helpers';
+	import InputHidden from '$lib/Kratos/InputHidden.svelte';
+	import InputDefault from '$lib/Kratos/InputDefault.svelte';
+	import InputPassword from '$lib/Kratos/InputPassword.svelte';
+	import SubmitButton from '$lib/Kratos/SubmitButton.svelte';
 
 	export let ui: UiContainer;
+	let nodes = getUiNodes(ui.nodes);
+
 </script>
 
 <AuthContainer flowType="verification">
@@ -53,6 +60,27 @@
 	</div>
 	<Message messages={ui.messages} />
 	<AuthForm formConfig={ui}>
-		<Input fields={ui.nodes} />
+		{#each nodes as { meta: { label }, attributes: { name, type, value, disabled } }}
+			{#if type === 'hidden'}
+				<InputHidden {name} {value} />
+			{/if}
+			{#if type === 'email'}
+				<InputDefault {name} {type} placeholder="E-Mail" value={value ? value : ''} {disabled} />
+			{/if}
+			{#if name === 'password'}
+				<InputPassword
+					{name}
+					{type}
+					placeholder={label?.text}
+					value={value ? value : ''}
+					{disabled}
+				/>
+			{/if}
+			{#if type === 'submit'}
+				<SubmitButton {name} {type} value={value ? value : ''} {disabled}>
+					{label?.text}
+				</SubmitButton>
+			{/if}
+		{/each}
 	</AuthForm>
 </AuthContainer>
