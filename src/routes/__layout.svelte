1c0023f3241ec8584197454006d841b9842c6e46
DeAndre Johnson <dre@phreshr.com>
2021-05-27 18:31:44 +0800

    removed nav on auth routes

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -7,14 +7,18 @@
 	export let path: string;
 
 	$: path = $page.path.slice(1);
+	let auth_route = $page.path.includes('auth');
+	console.log($page.path.includes('auth'));
 </script>
 
 <TailwindCSS />
 
 <InlineSvg />
 
-<div class="flex flex-col min-h-screen">
-	<Nav {path} />
+<div class="flex flex-col w-full min-h-screen">
+	{#if !$page.path.includes('auth')}
+		<Nav {path} />
+	{/if}
 
 	<main class="flex flex-col flex-1 justify-center items-center">
 		<slot />
