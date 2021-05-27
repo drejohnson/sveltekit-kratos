c3c6e6d40cf24010019ff13ddaf3172773420738
Dre Johnson <dre@phreshr.com>
2021-05-27 23:25:44 +0800

    refactor to display stack error

--- src/routes/__error.svelte
+++ src/routes/__error.svelte
@@ -1,15 +1,21 @@
 <script context="module">
 	export function load({ error, status }) {
 		return {
-			props: {
-				title: `${status}: ${error.message}`
-			}
+			props: { error, status }
 		};
 	}
+
 </script>
 
-<script>
-	export let title;
+<script lang="ts">
+	import { dev } from '$app/env';
+	export let error, status;
+	let title = `${status}`;
+
 </script>
 
 <h1>{title}</h1>
+
+{#if dev && error.stack}
+	<pre>{error.stack}</pre>
+{/if}
