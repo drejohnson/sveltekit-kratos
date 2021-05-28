8b1e187a9fc957e69148b3065f7a9726ed3bf686
DeAndre Johnson <dre@phreshr.com>
2021-05-28 13:02:49 +0800

    updated to display session info

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,6 +1,36 @@
-<script lang="ts">
-  // your script goes here
+<script lang="ts" context="module">
+	import type { Load } from '@sveltejs/kit';
+
+	export const load: Load = async ({ session }: { session: Session }) => {
+		if (session.user) {
+			return {
+				props: {
+					session
+				}
+			};
+		}
+
+		return {
+			status: 302,
+			redirect: '/auth/login'
+		};
+	};
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import type { Session } from '$lib/types';
+	export let session: Session;
+</script>
+
+<h2 class="text-3xl py-4">Hello, {session.user.username}</h2>
+
+<h3 class="text-xl py-4">Welcome to SvelteKit with Authentication via Ory Kratos</h3>
+<p class="pb-1">
+	Visit <a class="text-blue-600 underline" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the
+	sveltekit documentation
+</p>
+<p class="pb-1">
+	Visit <a class="text-blue-600 underline" href="https://www.ory.sh/kratos/docs/"
+		>www.ory.sh/kratos/docs</a
+	> to read the Ory Kratos documentation
+</p>
