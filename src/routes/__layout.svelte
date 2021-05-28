7f5b3461da68d6caf8901ce2f626fb7d10ba897d
DeAndre Johnson <dre@phreshr.com>
2021-05-28 13:00:50 +0800

    get session and pass it to nav component

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,14 +1,28 @@
+<script lang="ts" context="module">
+	import type { Load } from '@sveltejs/kit';
+	export const load: Load = ({ session }: { session: Session }) => {
+		if (session.user) {
+			return {
+				props: {
+					session
+				}
+			};
+		}
+		return {};
+	};
+</script>
+
 <script lang="ts">
 	import { page } from '$app/stores';
+	import type { Session } from '$lib/types';
 	import InlineSvg from '$lib/InlineSvg.svelte';
 	import Nav from '$lib/Nav.svelte';
 	import TailwindCSS from '$lib/TailwindCSS.svelte';
 
 	export let path: string;
+	export let session: Session;
 
 	$: path = $page.path.slice(1);
-	let auth_route = $page.path.includes('auth');
-	console.log($page.path.includes('auth'));
 </script>
 
 <TailwindCSS />
@@ -17,7 +31,7 @@
 
 <div class="flex flex-col w-full min-h-screen">
 	{#if !$page.path.includes('auth')}
-		<Nav {path} />
+		<Nav {path} {session} />
 	{/if}
 
 	<main class="flex flex-col flex-1 justify-center items-center">
