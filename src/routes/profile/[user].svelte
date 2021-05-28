23729b77fbfb693e43c5d5d78cbd5776197e5db6
DeAndre Johnson <dre@phreshr.com>
2021-05-28 13:04:53 +0800

    redirect to /profile/@username

---
+++ src/routes/profile/[user].svelte
@@ -0,0 +1,10 @@
+<script lang="ts" context="module">
+	import type { Load } from '@sveltejs/kit';
+
+	export const load: Load = async ({ session }) => {
+		return {
+			status: 302,
+			redirect: `/profile/@${session.user.username}`
+		};
+	};
+</script>
