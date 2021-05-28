df2f934861389d4e6829e81822f0b8c79f54c800
DeAndre Johnson <dre@phreshr.com>
2021-05-28 13:35:25 +0800

    updated profile page to display user info

--- src/routes/profile/@[user].svelte
+++ src/routes/profile/@[user].svelte
@@ -19,8 +19,18 @@
 
 <script lang="ts">
 	import type { Session } from '$lib/types';
+	import { logoutUrl } from '$lib/helpers';
 
 	export let session: Session;
 </script>
 
-<h1>Welcome {session.user.username}</h1>
+<h2 class="text-3xl py-4">Welcom back, {session.user.username}</h2>
+
+<h3 class="text-xl py-4">Your info:</h3>
+
+<pre><code>{JSON.stringify(session.user, null, 2)}</code></pre>
+
+<a
+	class="flex items-center justify-center h-12 px-6 w-64 bg-blue-600 mt-8 rounded font-semibold text-sm text-blue-100 hover:bg-blue-700"
+	href={logoutUrl}>Logout</a
+>
