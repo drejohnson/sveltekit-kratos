7b2ce652ccbf460eae4f42da51c180cfea4e4c76
DeAndre Johnson <dre@phreshr.com>
2021-05-28 12:59:59 +0800

    added session type

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -1,1 +1,10 @@
 export type KratosFlowType = 'registration' | 'login' | 'settings' | 'verification' | 'recovery';
+
+export interface Session {
+	user: {
+		email: string;
+		username: string;
+		first_name: string;
+		last_name: string;
+	};
+}
