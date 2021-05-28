42b79b05bed0f5b6d0a23c308b4e0c6d371d2afd
DeAndre Johnson <dre@phreshr.com>
2021-05-28 13:03:39 +0800

    added user profile page

---
+++ src/routes/profile/@[user].svelte
@@ -0,0 +1,26 @@
+<script lang="ts" context="module">
+	import type { Load } from '@sveltejs/kit';
+
+	export const load: Load = async ({ session }: { session: Session }) => {
+		if (session.user) {
+			return {
+				props: {
+					session
+				}
+			};
+		}
+
+		return {
+			status: 302,
+			redirect: '/auth/login'
+		};
+	};
+</script>
+
+<script lang="ts">
+	import type { Session } from '$lib/types';
+
+	export let session: Session;
+</script>
+
+<h1>Welcome {session.user.username}</h1>
