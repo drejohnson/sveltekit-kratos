cd7b68c415c5d37edd6ee5b3b75ce98fde679025
DeAndre Johnson <dre@phreshr.com>
2021-05-29 10:58:10 +0800

    added input label styles for setting page

--- src/lib/TailwindCSS.svelte
+++ src/lib/TailwindCSS.svelte
@@ -7,6 +7,9 @@
 		.field {
 			@apply relative flex mt-4;
 		}
+		.form-label_settings {
+			@apply flex items-center w-1/4 text-lg text-gray-500;
+		}
 		.input {
 			@apply flex items-center w-full h-12 px-4 bg-gray-200 rounded focus:outline-none focus:ring-2;
 		}
