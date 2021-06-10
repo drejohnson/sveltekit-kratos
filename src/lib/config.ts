714ee564ecd0780cd80885550c99cd852e22fb6b
Dre Johnson <dre@phreshr.com>
2021-06-10 22:23:27 +0800

    updated config to use environment variables

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,29 +1,22 @@
-export const BASE_URL = import.meta.env.VITE_BASE_URL || 'http://localhost:3000';
-export const KRATOS_BROWSER_URL = 'http://localhost:3000';
-export const KRATOS_ADMIN_URL = 'http://localhost:4434';
-export const KRATOS_PUBLIC_URL = 'http://localhost:4433';
-export const SECURITY_MODE_COOKIE = 'cookie';
-export const SECURITY_MODE_JWT = 'jwt';
-export const JWKS_URL = `${BASE_URL}/.well-known/jwks.json`;
-export const PROJECT_NAME = 'Sveltekit Kratos';
-
 type SecurityMode = 'cookie' | 'jwt';
 
-const baseUrl = BASE_URL;
+const baseUrl = import.meta.env.VITE_BASE_URL || '/';
 
-let securityMode: SecurityMode = 'cookie';
-let browserUrl = KRATOS_BROWSER_URL;
-let publicUrl = KRATOS_PUBLIC_URL;
+let securityMode: SecurityMode = import.meta.env.VITE_SECURITY_MODE || 'cookie';
+let browserUrl = import.meta.env.VITE_KRATOS_BROWSER_URL || 'http://localhost:3000';
+let publicUrl = import.meta.env.VITE_KRATOS_PUBLIC_URL;
+let adminUrl = import.meta.env.VITE_KRATOS_ADMIN_URL;
+let jwksUrl = import.meta.env.VITE_JWKS_URL || '/';
+let projectName = 'Sveltekit Kratos';
 
 export default {
 	kratos: {
 		browser: browserUrl,
-		admin: KRATOS_ADMIN_URL,
+		admin: adminUrl,
 		public: publicUrl
 	},
 	baseUrl,
-	jwksUrl: JWKS_URL,
-	projectName: PROJECT_NAME,
-
+	jwksUrl,
+	projectName,
 	securityMode
 };
