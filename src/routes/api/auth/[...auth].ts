13adead997ef7300cc7304b51a8e4e22ccd7aabf
Dre Johnson <dre@phreshr.com>
2021-06-17 23:43:55 +0800

    clean unused imports, etc

--- src/routes/api/auth/[...auth].ts
+++ src/routes/api/auth/[...auth].ts
@@ -1,15 +1,9 @@
 import type { Request } from '@sveltejs/kit';
-import { Configuration, AdminApi, ErrorResponse } from '@ory/kratos-client';
-import config from '$lib/config';
+import type { ErrorResponse } from '@ory/kratos-client';
 import type { AuthFlowType } from '$lib/types';
 import { kratosAdminApi } from '$lib/services/kratos';
 
-const configuration = new Configuration({
-	basePath: config.kratos.admin
-});
-
 export const get = async (req: Request) => {
-	const kratos = new AdminApi(configuration);
 	const flowId = req.headers.flow_id;
 	const error = req.headers.error;
 	const flowType = req.params.auth;
