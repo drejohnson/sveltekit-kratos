d9af58fcf939b0d915d84fa16f2e5ae91b3eab58
Dre Johnson <dre@phreshr.com>
2021-06-24 16:30:53 +0800

    cleaned up load function

--- src/routes/auth/_load.ts
+++ src/routes/auth/_load.ts
@@ -7,41 +7,26 @@ export const createLoad = (flowType: KratosFlowType) => {
 	const load: Load = async ({ page, fetch }) => {
 		const flowID = page.query.get('flow');
 
-		if (!flowID || !isString(flowID)) {
+		const res = await fetch(`/api/auth/${flowType}`, {
+			headers: {
+				flow_id: flowID
+			}
+		});
+
+		if (!res.ok) {
 			return {
-				redirect: `${config.kratos.public}/self-service/${flowType}/browser`,
-				status: 302
+				status: 302,
+				redirect: `${config.kratos.public}/self-service/${flowType}/browser`
 			};
 		}
 
-		try {
-			const res = await fetch(`/api/auth/${flowType}`, {
-				headers: {
-					flow_id: flowID
-				}
-			});
+		const { data: flow }: { status: number; data: AuthFlowType } = await res.json();
 
-			if (!res.ok) {
-				return {
-					status: 302,
-					redirect: `${config.kratos.public}/self-service/${flowType}/browser`
-				};
+		return {
+			props: {
+				ui: flow.ui
 			}
-
-			const { status, data: flow }: { status: number; data: AuthFlowType } = await res.json();
-
-			if (status !== 200) {
-				throw flow;
-			}
-
-			return {
-				props: {
-					ui: flow.ui
-				}
-			};
-		} catch (error) {
-			console.log(error);
-		}
+		};
 	};
 
 	return load;
