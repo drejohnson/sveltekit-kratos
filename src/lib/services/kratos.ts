6620119afe469474623b6083e7b5ee4dda8628e1
Dre Johnson <dre@phreshr.com>
2021-06-24 16:31:29 +0800

    added correct base path

--- src/lib/services/kratos.ts
+++ src/lib/services/kratos.ts
@@ -1,9 +1,14 @@
 import { Configuration, PublicApi, AdminApi } from '@ory/kratos-client';
 import config from '$lib/config';
 
-const configuration = new Configuration({
-	basePath: config.kratos.admin
-});
+export const kratosPublicApi = new PublicApi(
+	new Configuration({
+		basePath: config.kratos.public
+	})
+);
 
-export const kratosPublicApi = new PublicApi(configuration);
-export const kratosAdminApi = new AdminApi(configuration);
+export const kratosAdminApi = new AdminApi(
+	new Configuration({
+		basePath: config.kratos.admin
+	})
+);
